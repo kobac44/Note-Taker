d25fdcc19ecbc464c2ec6f14867077962e80a625
Unko <kobie.watkins@gmail.com>
2020-07-29 18:08:20 -0400

    using in class activities to facilitate needed code and reference

---
+++ api-route.js
@@ -0,0 +1,36 @@
+//methhods to use and think about
+//POST- Create, GET- Read, PUT- Update, DELETE- Delete..
+//CRUD
+//Need a Router defined
+//Need to store information
+
+const router = require("express").Router();
+// const store = require("./../db/store");
+
+// const
+
+// module.exports = function(app) {
+
+// //API Routes
+
+// // View the Route
+
+// //read the route
+//   app.get() {
+
+//   }
+// //create the route
+//   app.post {
+
+//   }
+
+// //read the route with an {id}
+//   app.get() {
+
+//   }
+
+//   app.delete() {
+
+//   }
+
+// }
