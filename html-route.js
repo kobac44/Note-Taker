90881c6e0f94ad05e28463948a7ba70417e55a35
Unko <kobie.watkins@gmail.com>
2020-07-30 23:10:18 -0400

    add and used code understanding from class notes

--- html-route.js
+++ html-route.js
@@ -1,19 +1,28 @@
-// const path = require("path");
+// DEPENDENCIES
+// needed for the correct file path
+const path = require("path");
 // const router = require("express").Router();
 
-// // Display notes.html when /notes is accessed
-// app.get("/notes", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public/notes.html"));
-// });
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
-// // Display index.html when all other routes are accessed
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
+module.exports = function (app) {
+  // Display notes.html when /notes is accessed
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-// //updates the json file whenever a note is added or deleted
-// function updateDb() {
-//   fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
-//     if (err) throw err;
-//     return true;
-//   })
+  // Display index.html when all other routes are accessed
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  //updates the json file whenever a note is added or deleted
+  function updateDb() {
+    fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
+      if (err) throw err;
+      return true;
+    });
+  }
+};
